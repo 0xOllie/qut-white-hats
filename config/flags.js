de4e7d5df5ab03600ddf4ce8ecb4cdc868a80910
yugen-yeap <36917057+yugen-yeap@users.noreply.github.com>
2020-07-24 16:49:48 +1000

    added two more easy challenges

--- config/flags.js
+++ config/flags.js
@@ -126,4 +126,39 @@ exports.flags = {
             flag: 'flag{its_better_with_two}',
         }
     },
+    challenge11: {
+        call: {
+            name: 'Challenge 11',
+            description: 'Nice and simple crack another ceasar salad?',
+            ciphertext: 'Iyeb myno sc kc cdbyxq kc k pybmo yp xkdebo. Kc novsmkdo kc k pvygob',
+        },
+        response:{
+            message: 'Congratulations!',
+            plaintext: 'Your code is as strong as a force of nature. As delicate as a flower',
+            flag: 'flag{love_for_coders?}',
+        }
+    },
+    challenge12: {
+        call: {
+            name: 'Challenge 12',
+            description: 'Looks like Josh wrote another love letter, but this time to Sarah.. what a romantic..',
+            ciphertext: `Xt zbwg eba tbd ys zyig mbfg. 
+            Cb zyxyos pag yc nzpmg.
+            Qdso zbpf xt fpop pcf pzzbjs 
+            oug ycsngmo edcmoybc be xt ugpao
+            ob adc sxbbouychzt
+            Jyzz tbd mbfg lpmi Agodac?`,
+        },
+        response:{
+            message: 'Congratulations!',
+            plaintext: `My love for you is like code. 
+            No limits are in place.
+            Just load my data and allows 
+            the inspect function of my heart
+            to run smoothingly
+            Will you code back Return?`,
+            flag: 'flag{thanks_for_the_poem_giselle}',
+            key: 'PLMFGEHUYQIZXCBNKASODWJRTV'
+        }
+    },
 }
